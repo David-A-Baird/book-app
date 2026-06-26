942869baf9ea5c7ea9fae7bc82feed8b3c641576
David-A-Baird <davandybair@gmail.com>
2026-06-26 14:15:17 -0600

    Removed characters component, updated about page and characters page and books page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { CharacterPages } from './pages/character-pages/character-pages';
-import { Characters } from './pages/characters/characters';
 import { Updates } from './pages/updates/updates';
 import { About } from './pages/about/about';
 import { Books } from './pages/books/books';
 
 export const routes: Routes = [
   { path: 'character', component: CharacterPages },
-  { path: 'characters', component: Characters },
   { path: 'books', component: Books },
   { path: 'updates', component: Updates },
   { path: 'about-the-author', component: About },
