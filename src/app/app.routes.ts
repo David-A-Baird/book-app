8a9095c5409a9152af9e25ec623589db271a7d89
David-A-Baird <davandybair@gmail.com>
2026-06-26 11:41:42 -0600

    Added header and footer components, added pages folder and components folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { CharacterPages } from './character-pages/character-pages';
-import { Characters } from './characters/characters';
-import { Updates } from './updates/updates';
-import { About } from './about/about';
-import { Books } from './books/books';
+import { CharacterPages } from './pages/character-pages/character-pages';
+import { Characters } from './pages/characters/characters';
+import { Updates } from './pages/updates/updates';
+import { About } from './pages/about/about';
+import { Books } from './pages/books/books';
 
 export const routes: Routes = [
   { path: 'character', component: CharacterPages },
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { routes } from './app/app.routes';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Header } from './app/components/header/header';
+import { Footer } from './app/components/footer/footer';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
